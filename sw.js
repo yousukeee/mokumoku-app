21f30441cf3a7f6912169e5208ff622db53c4953
yousukeee <63438226+yousukeee@users.noreply.github.com>
2026-05-21 01:55:31 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mokumoku-v1';
+const CACHE = 'mokumoku-v2';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json',
                 './icon-192.png', './icon-512.png'];
 
